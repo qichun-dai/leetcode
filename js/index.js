69a5cc7739040760621a62a8933296c1d6aebb2a
Qichun Dai <49536404+qichun-dai@users.noreply.github.com>
2021-09-22 10:03:25 +0800

    update plot

--- js/index.js
+++ js/index.js
@@ -1,13 +1,47 @@
 const csvUrl = "https://gist.githubusercontent.com/qichun-dai/c546d6273de202794b9cadedfbd86384/raw/33143dbb12e8dfabb8838d20a8790b493d6c5f7c/leetcode.csv";
+const formatTime = d3.timeFormat("%Y-%m-%d");
 
 const data = await d3.csv(csvUrl,(d) => {
     return {
-      Frequency:d.Frequency,
+      Frequency:parseInt(d.Frequency),
       Title: d.Title,
       Acceptance: d.Acceptance,
       Difficulty: d.Difficulty,
-      Date: new Date(d.Date), // convert "Year" column to Date
+      Date: new Date(d.Date), // convert Date to a certain format
+      DateText: formatTime(new Date(d.Date)),
     };
 });
 
-console.log(data[1]);
+const margin = {top: 25, right: 25, bottom: 25, left: 25};
+const width = 1200;
+const height = 500;
+
+const xScale = d3.scaleTime()
+.domain([d3.min(data, d => d.Date),d3.max(data, d => d.Date)])
+.range([margin.left,width-margin.right])
+.nice()
+
+const yScale = d3.scaleLinear()
+.domain(d3.extent(data, d => d.Frequency))
+.range([height-margin.bottom,margin.top])
+.nice()
+
+console.log(yScale(106));
+
+const svg = d3.select("div")
+.append("svg")
+.attr("width", width)
+.attr("height",height)
+.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+svg.selectAll("circle")
+.data(data)
+.enter()
+.append("circle")
+.attr("cx", d => xScale(d.Date)+(Math.random()-0.5)*10)
+.attr("cy",(d,i) => yScale((0.5-Math.random())*200)-200)
+.attr("r",3)
+.attr("opacity","0.5")
+.attr("fill",d => d.Difficulty)
+.append("title")
+.text(d => d.Title);
